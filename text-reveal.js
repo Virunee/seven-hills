8fecd9d86ad0cc5166665ae67ac99339d3b72473
Virunee <viravongv@gmail.com>
2017-07-05 00:48:53 +0100

    Massive visual and content changes

---
+++ text-reveal.js
@@ -0,0 +1,59 @@
+function RevealMoreBart()
+{
+   /* Makes content div height automatic and removes the fade-out div and more link. */
+   /* Specify the ID values of the content div, the fade-out div, and the more link. */
+   document.getElementById("fade-text-bart").style.height = "auto";
+   document.getElementById("fade-out-bart").style.display = "none";
+}
+function RevealLessBart() {
+   document.getElementById("fade-text-bart").style.height = "200px";
+  document.getElementById("fade-out-bart").style.display = "inherit";
+}
+
+function RevealMoreChris()
+{
+   /* Makes content div height automatic and removes the fade-out div and more link. */
+   /* Specify the ID values of the content div, the fade-out div, and the more link. */
+   document.getElementById("fade-text-chris").style.height = "auto";
+   document.getElementById("fade-out-chris").style.display = "none";
+}
+function RevealLessChris() {
+   document.getElementById("fade-text-chris").style.height = "200px";
+  document.getElementById("fade-out-chris").style.display = "inherit";
+}
+
+function RevealMoreMes()
+{
+   /* Makes content div height automatic and removes the fade-out div and more link. */
+   /* Specify the ID values of the content div, the fade-out div, and the more link. */
+   document.getElementById("fade-text-mes").style.height = "auto";
+   document.getElementById("fade-out-mes").style.display = "none";
+}
+function RevealLessMes() {
+   document.getElementById("fade-text-mes").style.height = "200px";
+  document.getElementById("fade-out-mes").style.display = "inherit";
+}
+
+function RevealMoreShaun()
+{
+   /* Makes content div height automatic and removes the fade-out div and more link. */
+   /* Specify the ID values of the content div, the fade-out div, and the more link. */
+   document.getElementById("fade-text-Shaun").style.height = "auto";
+   document.getElementById("fade-out-Shaun").style.display = "none";
+}
+function RevealLessShaun() {
+   document.getElementById("fade-text-Shaun").style.height = "200px";
+  document.getElementById("fade-out-Shaun").style.display = "inherit";
+}
+
+function RevealMoreSimon()
+{
+   /* Makes content div height automatic and removes the fade-out div and more link. */
+   /* Specify the ID values of the content div, the fade-out div, and the more link. */
+   document.getElementById("fade-text-Simon").style.height = "auto";
+   document.getElementById("fade-out-Simon").style.display = "none";
+}
+function RevealLessSimon() {
+   document.getElementById("fade-text-Simon").style.height = "200px";
+  document.getElementById("fade-out-Simon").style.display = "inherit";
+}
